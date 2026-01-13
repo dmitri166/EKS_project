3548c683aca2b949e6244a0bc69298d0f3591ae9
dmitri166 <dmitri@dima.localdomain>
2026-01-13 11:36:50 +0200

    Remove all Argo CD references - clean GitOps separation

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -8,11 +8,6 @@ output "eks_node_role_arn" {
   value       = aws_iam_role.eks_node_role.arn
 }
 
-output "argocd_role_arn" {
-  description = "ARN of the Argo CD role"
-  value       = aws_iam_role.argocd_role.arn
-}
-
 output "eks_cluster_role_name" {
   description = "Name of the EKS cluster role"
   value       = aws_iam_role.eks_cluster_role.name
@@ -21,9 +16,4 @@ output "eks_cluster_role_name" {
 output "eks_node_role_name" {
   description = "Name of the EKS node role"
   value       = aws_iam_role.eks_node_role.name
-}
-
-output "argocd_role_name" {
-  description = "Name of the Argo CD role"
-  value       = aws_iam_role.argocd_role.name
 }
