622962952b40cc20b7fb3510bc49101500ac75fc
dmitri <dmitri>
2026-04-03 14:08:53 +0300

    eks project modifactions

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -35,10 +35,13 @@ resource "aws_eks_node_group" "main" {
     min_size     = var.min_size
   }
 
-  instance_types = [var.instance_type]
+  instance_types = [
+    "t3.small",
+    "t3a.small"
+  ]
 
   ami_type = "AL2_x86_64"
-  capacity_type = "ON_DEMAND"
+  capacity_type = "SPOT"
   
   update_config {
     max_unavailable = 1
