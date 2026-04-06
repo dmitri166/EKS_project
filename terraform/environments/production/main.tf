2160d5868353c5e3fcbf43d5f069ff68fe476300
dmitri <dmitri>
2026-04-06 11:10:46 +0300

    fix: add Kubernetes and Helm providers for EKS
    - Configure kubernetes provider with EKS cluster endpoint
    - Configure helm provider for ArgoCD deployment
    - Add required providers to Terraform block
    - Fix 'connection refused' error when creating Kubernetes resources

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -9,6 +9,14 @@ terraform {
       source  = "hashicorp/http"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 
   backend "s3" {
@@ -76,6 +84,29 @@ module "eks" {
   tags                      = var.tags
 
   depends_on = [module.vpc, module.iam]
+}
+
+# Kubernetes and Helm providers for EKS
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+    }
+  }
 }
 
 # OIDC Module
